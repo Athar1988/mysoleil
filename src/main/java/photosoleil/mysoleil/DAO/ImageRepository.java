c9bc46ded62f0f75b685b496926b8a728d4fc3b7
Athar1988 <hakim.athar.88@gmail.com>
2021-10-26 08:20:16 +0100

    AJOUTER ENTITY IMAGE 4

--- src/main/java/photosoleil/mysoleil/DAO/ImageRepository.java
+++ src/main/java/photosoleil/mysoleil/DAO/ImageRepository.java
@@ -3,12 +3,15 @@ package photosoleil.mysoleil.DAO;
 
 
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import photosoleil.mysoleil.Entity.*;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-
+@CrossOrigin("*")
+@RepositoryRestResource
 public interface ImageRepository extends JpaRepository<ImageModel, Long> {
     Optional<ImageModel> findByName(String name);
 }
